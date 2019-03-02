5a8fff5fe3e40036f2f9690ec3c766a64c4d2d63
IvanDmitrachkov <idmitrachkovivan@aic.ru>
2019-03-02 11:21:30 +0300

    initial

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import s from './Header.module.scss'
+
+class Header extends Component {
+  render () {
+    return (
+      <div className={s.container}>
+        header
+      </div>
+    )
+  }
+}
+export default Header
