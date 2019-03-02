3dfc594696414d8b9f7cbbabd1345c50ed5cefa6
IvanDmitrachkov <idmitrachkovivan@aic.ru>
2019-03-02 22:57:59 +0300

    heroku

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import { Header, BlockWithFish, TextWithButton, Layout, TextWithButtonAndImage, OrderBlock } from 'exportComponents'
+import { Header, BlockWithFish, TextWithButton, Layout, TextWithButtonAndImage, OrderBlock, Modal } from 'exportComponents'
 import im6 from 'static/images/006.jpg'
+import { about, complect } from './data'
 
 class Main extends Component {
   render () {
@@ -9,7 +10,7 @@ class Main extends Component {
         <Header />
         <Layout>
           <BlockWithFish />
-          <TextWithButton button={<button>Подробнее об акваферме</button>}>
+          <TextWithButton button={<button onClick={() => {this.modalAbout.onOpenModal()}}>Подробнее об акваферме</button>}>
             <p>Акваферма – уникальный синтез аквариума и площадки для разведения растений.</p>
             <p>Акваферма представляет собой замкнутую экосистему, поддерживающую баланс между растениями и рыбкой. </p>
           </TextWithButton>
@@ -18,7 +19,7 @@ class Main extends Component {
             <p>Акваферма представляет собой замкнутую экосистему, поддерживающую баланс между растениями и рыбкой. </p>
           </TextWithButton>
           <TextWithButtonAndImage {...{
-            button: <button>Посмотреть комплект</button>,
+            button: <button onClick={() => {this.modalComplect.onOpenModal()}}>Посмотреть комплект</button>,
             image: im6
           }} >
             <p>В комплект входит все необходимое. <br />Вам остается добавить только рыбку</p>
@@ -27,7 +28,14 @@ class Main extends Component {
         <Layout bg={'#DFDFDF'}>
           <OrderBlock />
         </Layout>
-        <div style={{height: '200rem'}} />
+
+        <Modal ref={node => { if (node) this.modalAbout = node}} title={'Подробнее об акваферме'} >
+          {about}
+        </Modal>
+
+        <Modal ref={node => { if (node) this.modalComplect = node}} title={'Комплект Аквафермы'} >
+          {complect}
+        </Modal>
 
       </div>
     )
