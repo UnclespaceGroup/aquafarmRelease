fe0b41e570b989912ee322134c1c566f5144c9a4
IvanDmitrachkov <idmitrachkovivan@aic.ru>
2019-03-08 18:00:57 +0300

    fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import s from './Footer.module.scss'
+import logo from '../../static/images/logo.png'
 
 const Footer = () => {
   return (
     <div className={s.container}>
-      <a href={'tel:88005553535'} className={s.phone}>8 800 555 3535</a>
+      <div className={s.wrapper}>
+        <a href={'tel:88005553535'} className={s.phone}>8 800 555 3535</a>
+        <div className={s.logo} style={{backgroundImage: `url(${logo})`}} />
+      </div>
     </div>
   )
 }
